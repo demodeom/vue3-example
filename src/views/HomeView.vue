d91d637da2fbc1fde25cfd229227b13d58b768e3
demo <demodeom@example.com>
2025-05-27 00:24:56 +0800

    简单页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
+
+<!--javascript 代码-->
 <script setup>
 
 </script>
 
+<!--html 代码-->
 <template>
   <h1>这里是首页</h1>
 </template>
+
+<!--css 代码-->
+<style scoped>
+h1{
+  color: red;
+  font-weight: bold;
+}
+</style>
