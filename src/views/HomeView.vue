91fabb4205ffe9808a365da9a900b70789a8f801
demo <demodeom@example.com>
2025-05-31 00:12:43 +0800

    Example 02

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,95 +1,88 @@
 <!--javascript 代码-->
 <script setup>
 /**
- * 1. 定义响应式数据
- *    1.1 基础数据类型 使用 ref()
- *    1.2 非基础数据类型 使用 reactive()
- * 2. 文本插值
+ * 复杂数据类型渲染：
  *
- * 3. 事件绑定， 比如点击事件
- *
- * 4. 函数定义
- *
- * 5. 修改响应式数据值
+ * 1. 数组
  */
 
-import { reactive, ref } from 'vue'
+import { reactive } from 'vue'
 
-// 定义基础响应式数据
-const num = ref(0)
+const jobs = reactive(['吃饭', '睡觉', '打豆豆'])
 
-// 定义非基础(对象)数据类型
-const userInfo = reactive({
-  username: '张三',
-  userAge: 18,
-  userSex: '女',
-})
-
-// 函数定义-箭头函数
-const fun1 = () => {
-  alert('我被点击了')
-}
-
-// 函数定义-关键字function
-const increaseNum = function () {
-  num.value = num.value + 1
-}
-
-const decreaseNum = () => {
-  // 将 num 值 + 1
-  num.value = num.value - 1
-}
-
-
-const changeAge = function () {
-  /**
-   * 修改对象的属性值
-   * 直接使用属性名进行修改， 不需要在后面添加  value 属性
-   */
-  userInfo.userAge = 20
-}
-
+const users = reactive([
+  {
+    id: '2020120101',
+    name: '用户1',
+    sex: '保密',
+  },
+  {
+    id: '2020120102',
+    name: '用户2',
+    sex: '男',
+  },
+  {
+    id: '2020120103',
+    name: '用户3',
+    sex: '女',
+  },
+])
 </script>
 
 <!--html 代码-->
 <template>
-  <h1>这里是首页</h1>
+  <ul>
+    <li v-for="(item, index) in jobs" v-bind:key="index">{{ index }} - {{ item }}</li>
+  </ul>
 
-  <!--文本查值-->
-  <span>{{ num }}</span>
-  <!--  事件绑定 v-on:click-->
-  <button v-on:click="fun1">按钮</button>
-  <button v-on:click="increaseNum">增加</button>
-  <button v-on:click="decreaseNum">减少</button>
+  <table>
+    <thead>
+      <tr>
+        <th>学号</th>
+        <th>姓名</th>
+        <th>性别</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(user, index) in users" v-bind:key="index">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.sex }}</td>
+      </tr>
+    </tbody>
+  </table>
   <hr>
   <table>
-    <tr>
-      <td>姓名：</td>
-      <td> {{ userInfo.username }} </td>
-    </tr>
-    <tr>
-      <td>年龄：</td>
-      <td> {{ userInfo.userAge }} </td>
-    </tr>
-    <tr>
-      <td>性别：</td>
-      <td> {{ userInfo.userSex }} </td>
-    </tr>
+    <thead>
+      <tr>
+        <th>学号</th>
+        <th>姓名</th>
+        <th>性别</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(user, index) in users" v-bind:key="index">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+
+        <td v-if="user.sex === '男'" style="color: blue">{{ user.sex }}</td>
+        <td v-else-if="user.sex === '女'" style="color: red">{{ user.sex }}</td>
+        <td v-else>{{ user.sex }}</td>
+      </tr>
+    </tbody>
   </table>
-
-  <br>
-  <button v-on:click="changeAge">修改年龄为 20 岁</button>
-
 </template>
 
 <!--css 代码-->
 <style scoped>
-h1 {
-  color: red;
-  font-weight: bold;
+table {
+  width: 800px;
+  text-align: center;
+  border-spacing: 0;
 }
 
-button {
-  margin: 0 20px;
+th,
+td {
+  border: 1px solid black;
 }
 </style>
